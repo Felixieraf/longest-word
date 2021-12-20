cad2faf79fd12062efcb69c500681b99669b0cf1
RAFALIMANANA Felixie <dhaliahfeli@gmail.com>
2021-12-20 15:40:10 +0300

    FEATURE COMPLETE: dicitonary check of attempt

--- game.py
+++ game.py
@@ -2,6 +2,7 @@
 # pylint: disable=missing-docstring
 import  random
 import string
+import requests
 class Game:
     def __init__(self):
         self.grid = []
@@ -33,4 +34,10 @@ class Game:
                 letters.remove(letter)
             else:
                 return False
-        return True
+        return self.__check_dictionary(word)
+    
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
